7988ed50d23512f54a88feea321e2625b4a69241
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-30 14:02:20 +0530

    Added Name to Site

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
-
+import Link from "next/link";
 function Navbar() {
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+        <div className="max-w-7xl mx-auto px-4">
+            <div className="flex items-center justify-between h-16">
+                <div className="flex items-center">
+                    <Link href="/" className="">
+                        Winpedia
+                    </Link>
+                </div>
+            </div>
+        </div>
 
-        
     </nav>
   )
 }
