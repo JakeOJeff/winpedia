2270229b31f730951ad890118ee5e01e6c8efed7
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-12-01 03:44:29 +0530

    Added avatar and text area for CreatePost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 
 import { currentUser } from "@clerk/nextjs/server";
+import CreatePost from "@/components/CreatePost";
 
 export default async function Home() {
   const user = await currentUser();
@@ -7,7 +8,13 @@ export default async function Home() {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+        <div className="lg:col-span-6">
+          {user ? <CreatePost /> : (<></>)}
+        </div>
 
+        <div className="hidden lg:block lg:col-span-4 sticky top-20">
+          WhoToFollow
+        </div>
     </div>
   );
 }
