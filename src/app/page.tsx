a81b6de12342d56180e95ef6672a5c7f906af617
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-30 12:18:18 +0530

    Fixing packaging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,16 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/nextjs'
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode="modal"/>
+        <SignInButton mode="modal">
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
         <SignUpButton mode="modal">
           <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
             Sign Up
