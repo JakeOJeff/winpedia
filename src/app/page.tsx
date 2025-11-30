186408bb57807cbd4371e302bb57f6a6736b5842
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-30 12:09:41 +0530

    Adding Sign In modals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton />
-        <SignUpButton>
+        <SignInButton mode="modal"/>
+        <SignUpButton mode="modal">
           <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
             Sign Up
           </button>
