619a923c996ec11037026652b62ff3f5636ecc65
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-12-01 06:18:41 +0530

    Fix Issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,24 +8,24 @@ import { getDbUserId } from "@/actions/user.action";
 
 export default async function Home() {
   const user = await currentUser();
-  const posts = await getPosts()
-  const dbUserId = await getDbUserId()
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-        <div className="lg:col-span-6">
-          {user ? <CreatePost /> : (<></>)}
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
 
-          <div className="space-y-6">
-            {posts.map(post) => (
-              <PostCard key={posts.id} post={post} dbUserId={dbUserId} />
-            )}
-          </div>
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
         </div>
+      </div>
 
-        <div className="hidden lg:block lg:col-span-4 sticky top-20">
-          <RecommendedUsers />
-        </div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <RecommendedUsers />
+      </div>
     </div>
   );
 }
