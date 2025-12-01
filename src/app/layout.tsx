ede792db1d0dcd27ba1f009a2aa631c880283bc0
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-12-01 22:12:50 +0530

    Working Snowfall effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
               particleCount={200}
               particleSpread={10}
               speed={0.1}
-              particleBaseSize={50}
+              particleBaseSize={100}
               moveParticlesOnHover={false}
               alphaParticles={false}
               disableRotation={true}
