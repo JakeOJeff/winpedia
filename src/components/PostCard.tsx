b0acd2314749a3ac23b0588988c3cd6ab01a9224
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-12-01 05:59:04 +0530

    Like handle logic

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { getPosts } from "@/actions/post.action";
+import { getPosts, toggleLike } from "@/actions/post.action";
 import { useUser } from "@clerk/nextjs";
 import { useState } from "react";
 
@@ -12,9 +12,28 @@ function PostCard({post, dbUserId}: { post: Post; dbUserId: string}) {
     const [ isCommenting, setIsCommenting ] = useState(false);
     const [ isLiking, setIsLiking ] = useState(false);
     const [ isDeleting, setIsDeleting ] = useState(false)
-    const [ hasLiked, setHasLiked ] = useState(false)
+    const [ hasLiked, setHasLiked ] = useState(post.likes.some(like => like.userId === dbUserId))
     const [ optimisticLikes, setOptimisticLikes ] = useState(post._count.likes)
-  return (
+  
+    const handleLike = async () => {
+        if (isLiking) return
+        setIsLiking(true)
+        try {
+            setIsLiking(true)
+            setHasLiked(prev => !prev)
+            setOptimisticLikes(prev => prev + (hasLiked ? -1 : 1))
+            await toggleLike(post.id)
+        } catch (error) {
+            setOptimisticLikes(post._count.likes)
+            setHasLiked(post.likes.some(like => like.userId === dbUserId))
+
+        } finally {
+            setIsLiking(false)
+        }
+    }
+  
+  
+    return (
     <div>
       
     </div>
