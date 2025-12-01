350aae2df6fb7413292ba83908914e4a36335889
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-12-01 21:25:10 +0530

    Setting Dropzone

---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { UploadDropzone } from "@uploadthing/react";
+
+interface ImageUploadProps {
+    onChange: (url:string) => void
+    value: string;
+    endpoint: "postImage"
+}
+
+function ImageUpload({endpoint, onChange, value}:ImageUploadProps) {
+  return (
+    <UploadDropzone
+        endpoint={endpoint}
+        onClientUploadComplete={(res) =>{
+                onChange(res?.[0].url)
+        }}
+        onUploadError={(error: Error) => {
+            console.log(error);
+        }}
+    />
+  )
+}
+
+export default ImageUpload
